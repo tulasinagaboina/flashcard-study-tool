b940f5be74a2281a96045030ccaa2ceedba99428
Tulasi <tulasinagaboina@gmail.com>
2025-02-25 19:06:57 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello,I'm Tulsi.learning React Course!</p>
+        <h2>My Journey with React</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
